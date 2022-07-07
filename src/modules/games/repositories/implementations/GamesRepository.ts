ce67425eb6c0c4e9dd06b8faf260db65505d6b65
Dener Fernandes <dener.fernandes.oliveira@gmail.com>
2022-07-06 23:27:38 -0400

    App com erro

---
+++ src/modules/games/repositories/implementations/GamesRepository.ts
@@ -0,0 +1,37 @@
+import { getRepository, Repository } from 'typeorm';
+
+import { User } from '../../../users/entities/User';
+import { Game } from '../../entities/Game';
+
+import { IGamesRepository } from '../IGamesRepository';
+
+export class GamesRepository implements IGamesRepository {
+  private repository: Repository<Game>;
+
+  constructor() {
+    this.repository = getRepository(Game);
+  }
+
+  async findByTitleContaining(param: string): Promise<Game[]> {
+    return await this.repository
+      .createQueryBuilder("games")
+      .where("games.title ILIKE '%' || :id || '%'", {id: param})
+      .getMany();
+      // Complete usando query builder
+  }
+
+  async countAllGames(): Promise<[{ count: string }]> {
+    return await this.repository.query("SELECT COUNT(title) FROM games"); // Complete usando raw query
+  }
+
+  async findUsersByGameId(id: string): Promise<User[]> {
+    const usersList = await this.repository
+      .createQueryBuilder("games")
+      .leftJoinAndSelect("games.users", "user")
+      .where("games.id == :id", { id })
+      .getMany()
+
+      return usersList[0].users;
+      // Complete usando query builder
+  }
+}
